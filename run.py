a80c0e15d8423d21f47c5f6a7bdca0b2c27b7c7a
Bruna-Andelieri <bruna.andelieri@gmail.com>
2023-08-01 21:11:59 +0100

    Feat: test game logic

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+right = []
+wrong = []
+letter = ''
+word = 'azul'
+ATTEMPTS = 6
+while True:
+    letter = str(input('Type a letter: '))
+
+    if letter == word:
+        print('You won!')
+        break
+
+    if letter in word:
+        right.append(letter)
+    else:
+        wrong.append(letter)
+    
+    if ATTEMPTS == len(wrong):
+        print('You lose!')
+        break
+
+    if len(right) == len(word):
+        print('You won!')
+        break
+
+    print('right: ', right)
+    print('wrong: ', wrong)
+
+    
