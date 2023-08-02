31dcebd738c232715632463c8de7d24ee6a6de25
Bruna-Andelieri <bruna.andelieri@gmail.com>
2023-08-02 21:00:03 +0100

    Feat: add run function

--- run.py
+++ run.py
@@ -51,49 +51,57 @@ def is_special_character(letter):
         return False
 
 
+def run():
+    win = False
+    while True:
+        word = get_random_value(metal_bands)
 
-while True:
-    word = get_random_value(metal_bands)
+        victory = win_condition(word)
+        if victory:
+            win = True
+            print('You won!', f"Do you like {word}?")
+            break
+            
+        if ATTEMPTS == len(wrong):
+            win = False
+            print('You lose!', f"The band is {word}")
+            break
 
-    victory = win_condition(word)
-    if victory:
-        print('You won!', f"Do you like {word}?")
-        break
+        letter = input('Type a letter / Word: ').strip()
+
+        if not letter:
+            print('Spaces are not alowed! Please type again')
+            continue
+
+        check_double_letter = is_duplicated(letter)
+        if check_double_letter:
+            print('This letter was alredy used! Please type again')
+            continue
+
+        check_special_character = is_special_character(letter)
+        if check_special_character:
+            print('This is a special character! Please type again')
+            continue
+
+        if str(letter).isnumeric():
+            print('This is a number, type a letter! Please type again')
+            continue
+
+        check_letter = is_letter(letter)
+        if not check_letter and len(letter) == 1:
+            print('This is not a letter! Please type again')
+            continue
+
+        if letter in word:
+            right.append(letter)
+        else:
+            wrong.append(letter)
         
-    if ATTEMPTS == len(wrong):
-        print('You lose!', f"The band is {word}")
-        break
 
-    letter = input('Type a letter / Word: ').strip()
+        print('right: ', ", ".join(right))
+        print('wrong: ', ", ".join(wrong))
 
-    if not letter:
-        print('Spaces are not alowed! Please type again')
-        continue
+    return win
 
-    check_double_letter = is_duplicated(letter)
-    if check_double_letter:
-        print('This letter was alredy used! Please type again')
-        continue
 
-    check_special_character = is_special_character(letter)
-    if check_special_character:
-        print('This is a special character! Please type again')
-        continue
-
-    if str(letter).isnumeric():
-        print('This is a number, type a letter! Please type again')
-        continue
-
-    check_letter = is_letter(letter)
-    if not check_letter and len(letter) == 1:
-        print('This is not a letter! Please type again')
-        continue
-
-    if letter in word:
-        right.append(letter)
-    else:
-        wrong.append(letter)
-    
-
-    print('right: ', right)
-    print('wrong: ', wrong)
+run()
