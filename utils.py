09502a044344113958ab543af3eb8048b23bb196
Bruna-Andelieri <bruna.andelieri@gmail.com>
2023-08-09 09:59:05 +0100

    Fix: add docstring to utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
-import os
+"""
+This file contains the clear function, allowing other files
+to access it without needing to rewrite it.
+"""
 
-ROWSIZE = 80
+import os
 
 
 def clear_terminal():
+    """
+    clear the terminal
+    """
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
-
